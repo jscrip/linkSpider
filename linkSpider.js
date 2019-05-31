dee65c07d34a5616ba0d19f5f1793e8f5532bea8
Jeremy B <jscrip@users.noreply.github.com>
2019-05-30 21:20:01 -0400

    Create linkSpider.js

---
+++ linkSpider.js
@@ -0,0 +1,75 @@
+*/
+class linkSpider {
+  constructor() {
+    this.newLinks = [];
+    this.visitedLinks = [];
+    this.CUTOFF = 10000;
+    this.domain = window.location.origin;
+    this.startPage = window.location.href; //start from the current page
+    //this.startPage = domain;  //start from the home page
+    
+    this.linkCollection = {
+    	internal: [],
+    	external: []
+    }
+  }
+
+  getLinks(doc){
+  	return [...doc.querySelectorAll('a')].reduce((collection,link) => {
+  		collection.push({
+  			title:link.title,
+  			text:link.innerText.replace(/[^a-z0-9\s]/gim,"").replace(/[\s]+/gim," "),
+  			href:link.href
+  		})
+  		return collection;
+  	}, []);
+  }
+
+  parse(htmlString){
+  		var parser = new DOMParser();
+  		// Parse the text
+  		var doc = parser.parseFromString(htmlString, "text/html");
+  		var links = this.getLinks(doc);
+  		links.filter(link => this.visitedLinks.indexOf(link.href) == -1).forEach(link => {
+  				if(this.newLinks.indexOf(link.href) == -1){
+  						if(link.href.indexOf(this.domain) > -1){
+  							this.linkCollection.internal.push(link);
+  							this.newLinks.push(link.href);
+  						}else{
+  							this.linkCollection.external.push(link);
+  						}
+  				}
+  				this.visitedLinks.push(link.href);
+  		})
+  }
+
+async fetchURL(url,options){
+  try {
+  	var response = await fetch(url, options);
+    if(response.status == 200){
+      var htmlString = await response.text();
+      this.parse(htmlString,url);
+    }else{
+      console.log("ERROR: Server returned status:",response.status);
+    }
+
+  }
+  catch(error) {
+  	console.error(error);
+  }
+  if(this.newLinks.length > 0 && this.visitedLinks.length < this.CUTOFF){
+  	var newLink = this.newLinks.shift();
+  	this.fetchURL(newLink)
+  }else{
+      if(this.visitedLinks.length >= this.CUTOFF){
+        console.log("CUTOFF Reached");
+      }else{
+        console.log("DONE",this.linkCollection)
+      }
+  				return true;
+  		}
+  }
+
+}
+var runLinkSpider = new linkSpider();
+runLinkSpider.fetchURL(window.location.href);
